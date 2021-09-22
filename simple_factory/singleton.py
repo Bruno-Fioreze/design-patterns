a9aa5b156560dde2a7a756c7ecfe3a54eec19999
= <=>
2021-09-22 19:20:40 -0300

    Estudando Singleton.

--- simple_factory/singleton.py
+++
@@ -1,2 +0,0 @@
-class AppSettings:
-    _instance = None
--- singleton/singleton.py
+++ singleton/singleton.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     as1 = AppSettings()
     as2 = AppSettings()
 
-    print(as1)
-    print(as2)
+    print(as1 == as2)
+    print(id(as1) == id(as2))
 
